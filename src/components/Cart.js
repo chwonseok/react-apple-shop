9128428cc5f19ed74f9e249ea952aaa3838a1f06
cws0316 <dev.chwonseok@gmail.com>
2021-08-12 23:11:34 +0900

    Added more reducers

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,11 @@ import { Table } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 function Cart(props) {
+  console.log(props);
+  function alertHandler() {
+    return props.dispatch({ type: 'alert' });
+  }
+
   function plusQuantity() {
     return props.dispatch({ type: 'plus' });
   }
@@ -11,47 +16,51 @@ function Cart(props) {
   }
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Quantity</th>
-          <th>Change</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.state.map((cur, i) => {
-          return (
-            <tr key={i}>
-              <td>{cur.id}</td>
-              <td>{cur.name}</td>
-              <td>{cur.quantity}</td>
-              <td>
-                <button onClick={plusQuantity}>+</button>
-                <button onClick={minusQuantity}>-</button>
-              </td>
-            </tr>
-          );
-        })}
-        {/* <tr>
-          <td>{props.state[0].name}</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr> */}
-      </tbody>
-    </Table>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.items.map((cur, i) => {
+            return (
+              <tr key={i}>
+                <td>{cur.id}</td>
+                <td>{cur.name}</td>
+                <td>{cur.quantity}</td>
+                <td>
+                  <button onClick={plusQuantity}>+</button>
+                  <button onClick={minusQuantity}>-</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      {props.alert ? (
+        <div>
+          <p>It's on sale right now with 20% off!</p>
+          <button onClick={alertHandler}>Close</button>
+        </div>
+      ) : null}
+    </>
   );
 }
 
 // ⬇️: redux store에 있는 데이터를 가져와서 props로 변환해주는 함수
 // 아래의 경우 state는 store에 있는 모든 Data
-function funcName(state) {
+function stateToProps(state) {
   return {
-    state: state,
+    items: state.itemReducer,
+    alert: state.alertReducer,
   };
 }
 
-export default connect(funcName)(Cart);
-// export default Cart <-- 이렇게 처리해줘야 함!
+export default connect(stateToProps)(Cart);
+// redux 기능을 사용하기 위해선, 일반적인 export default Cart가 아닌..
+// ..위 처럼 해야함
--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ import App from './App';
 // STEP 1. Redux 세팅 ⬇️
 // Provider에 포함된 component는 props 체계없이 state 공유 가능
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const defaultState = [
+const alertState = true;
+
+function alertReducer(state = alertState, action) {
+  if (action.type === 'alert') {
+    state = false;
+    return state;
+  } else {
+    return state;
+  }
+}
+
+const itemState = [
   { id: 0, name: 'nice shoes', quantity: 2 },
   { id: 1, name: 'cool shoes', quantity: 5 },
 ];
-const store = createStore(reducer);
+
 // STEP 2. state 수정
 // ➡️ Redux를 이용한 state 수정 방법!!! Redux에선 state 데이터의 수정방법을 미리 정의해야 함 ⬇️
 // reducer는 항상 state를 퉤 뱉어야 함
-function reducer(state = defaultState, action) {
+function itemReducer(state = itemState, action) {
   if (action.type === 'plus') {
     const statePlus = [...state];
     statePlus[0].quantity++;
@@ -32,6 +43,9 @@ function reducer(state = defaultState, action) {
     return state;
   }
 }
+
+// STEP 3. reducer(state변경 방식)의 결과(return 값)을 store에 넣어줌
+const store = createStore(combineReducers({ itemReducer, alertReducer }));
 
 ReactDOM.render(
   <BrowserRouter>
